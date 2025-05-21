3f2fca2a12d276728a947612da648d3974267c74
Aashish Jha <aashishjha1107@gmail.com>
2025-05-21 05:54:09 +0530

    APP

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/layout/Layout';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-surface-50">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
